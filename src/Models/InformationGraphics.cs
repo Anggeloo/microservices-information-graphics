bbb49a00965dc6e754a09e22a57b6afe0bb67a5a
Anggelo <anstalyn1999@gmail.com>
2025-02-11 18:26:36 -0500

    feat: :wrench: Creating the base architecture of the microservice with organization in src/

---
+++ src/Models/InformationGraphics.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace microservices_information_graphics.Models
+{
+    [Table("information_graphics")]
+    public class InformationGraphic
+    {
+        [Key]
+        [Column("graphic_id")]
+        public int GraphicId { get; set; }
+
+        [Required]
+        [Column("graphic_code")]
+        [StringLength(100)]
+        public string GraphicCode { get; set; }
+
+        [Required]
+        [Column("graphic_type")]
+        [StringLength(50)]
+        public string GraphicType { get; set; }
+
+        [Required]
+        [Column("title")]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Column("description")]
+        public string? Description { get; set; }
+
+        [Column("user_created_by")]
+        [StringLength(100)]
+        public string UserCreatedBy { get; set; }
+
+        [Column("status")]
+        public bool Status { get; set; } = true;
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
